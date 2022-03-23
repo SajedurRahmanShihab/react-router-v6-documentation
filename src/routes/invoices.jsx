fe20235fc9a75ea8d6959cfe31031f4d43b20943
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2022-03-24 04:44:54 +0600

    search params

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,8 +1,14 @@
-import { Link, NavLink, Outlet } from "react-router-dom";
+import {
+    NavLink,
+    Outlet,
+    useSearchParams,
+} from "react-router-dom";
 import { getInvoices } from "../data";
 
 export default function Invoices() {
     let invoices = getInvoices();
+    let [searchParams, setSearchParams] = useSearchParams();
+
     return (
         <div style={{ display: "flex" }}>
             <nav
@@ -11,24 +17,40 @@ export default function Invoices() {
                     padding: "1rem",
                 }}
             >
-                {invoices.map((invoice) => (
-                    <NavLink
-                        style={({ isActive }) => {
-                            return {
-                                display: 'block',
-                                margin: '1rem 0',
-                                color: isActive ? 'red' : '',
-                                textDecoration: 'none'
-                            }
-                        }}
-                        to={`/invoices/${invoice.number}`}
-                        key={invoice.number}
-                    >
-                        {invoice.name}
-                    </NavLink>
-                ))}
+                <input
+                    value={searchParams.get("filter") || ""}
+                    onChange={(event) => {
+                        let filter = event.target.value;
+                        if (filter) {
+                            setSearchParams({ filter });
+                        } else {
+                            setSearchParams({});
+                        }
+                    }}
+                />
+                {invoices
+                    .filter((invoice) => {
+                        let filter = searchParams.get("filter");
+                        if (!filter) return true;
+                        let name = invoice.name.toLowerCase();
+                        return name.startsWith(filter.toLowerCase());
+                    })
+                    .map((invoice) => (
+                        <NavLink
+                            style={({ isActive }) => ({
+                                textDecoration: 'none',
+                                display: "block",
+                                margin: "1rem 0",
+                                color: isActive ? "red" : "",
+                            })}
+                            to={`/invoices/${invoice.number}`}
+                            key={invoice.number}
+                        >
+                            {invoice.name}
+                        </NavLink>
+                    ))}
             </nav>
             <Outlet />
-        </div >
+        </div>
     );
 }
